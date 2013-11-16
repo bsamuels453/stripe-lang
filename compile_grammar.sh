c2071cf460465e9b7a3918a61d08ff5854134711
FrankBro <brodeur_francois@hotmail.com>
2013-11-15 20:14:51 -0500

    Restructure, should be final now

---
+++ compile_grammar.sh
@@ -0,0 +1,9 @@
+export ANTLR4_LOCATION=$PWD/lib/antlr-4.1-complete.jar
+
+export CLASSPATH=".:$ANTLR4_LOCATION:$CLASSPATH"
+
+alias antlr4='java -jar $ANTLR4_LOCATION'
+alias grun='java org.antlr.v4.runtime.misc.TestRig'
+
+cd src/frontend/grammar
+java -jar $ANTLR4_LOCATION stripe.g4
---
+++ src/Stripe.java
@@ -0,0 +1,26 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: frankbro
+ * Date: 15/11/13
+ * Time: 3:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import grammar.*;
+
+public class Stripe {
+    public static void main(String[] args) throws Exception {
+
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+        stripeLexer lexer = new stripeLexer(input);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        stripeParser parser = new stripeParser(tokens);
+
+        ParseTree tree = parser.compilationUnit();
+        System.out.println(tree.toStringTree(parser));
+    }
+}

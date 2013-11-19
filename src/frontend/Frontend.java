5f1367b8901d8a6cbe54afb0527d66e2f26e395d
FrankBro <brodeur_francois@hotmail.com>
2013-11-19 00:03:52 -0500

    Mostly ideas about the development of the frontend

--- src/frontend/Frontend.java
+++ src/frontend/Frontend.java
@@ -6,6 +6,14 @@ package frontend;
  * Date: 15/11/13
  * Time: 9:27 PM
  * To change this template use File | Settings | File Templates.
+ *
+ *
+ * Lex -> Parse -> AST -> Simplify AST / Desugar ->
+ * Import additional symbols -> Resolve references ->
+ * Generate IR -> Optimize IR -> Generate Machine Code ->
+ * Optimize Machine code.
+ *
+ *
  */
 
 import org.antlr.v4.runtime.*;
@@ -19,11 +27,61 @@ public class Frontend {
         stripeLexer lexer = new stripeLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         stripeParser parser = new stripeParser(tokens);
-        ParseTree tree = parser.compilationUnit();
 
-        ParseTreeWalker walker = new ParseTreeWalker();
+        // Zeroth pass will be done by Antlr
+        ParseTree tree = parser.compilationUnit();
+        // At this point, the syntax is valid
+
+        tree = firstPass(tree);
+        tree = secondPass(tree);
+        tree = thirdPass(tree);
+        tree = forthPass(tree);
+        tree = fifthPass(tree);
 
         System.out.println(tree.toStringTree(parser));
+    }
+
+    // The first pass will only check that every identifiers are ok.
+    // They are either :
+    //      1- Defined locally within the file
+    //      2- Imported from another file
+    private ParseTree firstPass(ParseTree tree) {
+
+        return tree;
+    }
+
+    // The second pass will check for types
+    // This involves :
+    //      1- Comparing the types using in functions with the types expected
+    //      2- Generate what a declaration should return
+    private ParseTree secondPass(ParseTree tree) {
+
+        return tree;
+    }
+
+    // The third pass will check for contracts
+    // In the future, the compiler should try to optimize out the contacts
+    // Meaning, based on usage, the compiler should know if a contract can be broken
+    // if it can't, then it should be remove.
+    // For now, I will integrate them into the ast, like simple if's and such
+    // and call a function that will break the vm if its ever called.
+    private ParseTree thirdPass(ParseTree tree) {
+
+        return tree;
+    }
+
+    // The forth pass will actually import from other files
+    // For the first version, only single file compilation will be supported
+    private ParseTree forthPass(ParseTree tree) {
+
+        return tree;
+    }
+
+    // The fifth pass will involve a last optimization on the ast
+    // after that, its code generation
+    private ParseTree fifthPass(ParseTree tree) {
+
+        return tree;
     }
 
     // -------------------- TEST ---------------------------------------------
